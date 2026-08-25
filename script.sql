858c7ee28929dec2c16419bd87097ca4fb6433ae
Mariana Meneses <marianamenesesborgesdacosta@gmail.com>
2026-08-25 10:56:59 -0300

    learn(ap01-dw): encerra o codigo 4
    - ja fizemos a carga de dados na tabela raw
    - criamos a tabela de staging
    - proximo passo sera a carga na staging passando pelo T (transform)

--- script.sql
+++ script.sql
@@ -1,23 +1,46 @@
-DROP TABLE IF EXISTS raw.sales CASCADE;
-CREATE TABLE raw.sales(
-	invoice_id TEXT,
-	branch TEXT,
-	city TEXT,
-	customer_type TEXT,
-	gender TEXT,
-	product_line TEXT,
-	unit_price TEXT,
-	quantity TEXT,
-	tax_5pct TEXT,
-	total TEXT,
-	sale_date TEXT,
-	sale_time TEXT,
-	payment TEXT,
-	cogs TEXT,
-	gross_margin_percentage TEXT,
-	gross_income TEXT,
-	rating TEXT
-	);
+-- DROP TABLE IF EXISTS staging.sales;
+-- CREATE TABLE staging.sales(
+-- 	invoice_id VARCHAR (200) PRIMARY KEY,
+-- 	branch CHAR (1) NOT NULL,
+-- 	city VARCHAR (200) NOT NULL,
+-- 	customer_type VARCHAR (200) NOT NULL,
+-- 	gender VARCHAR(200) NOT NULL,
+-- 	product_line VARCHAR (200) NOT NULL,
+-- 	unit_price NUMERIC (10,2) NOT NULL,
+-- 	quantity INTEGER NOT NULL,
+-- 	tax_5pct NUMERIC (10,4) NOT NULL,
+-- 	total NUMERIC (12,2) NOT NULL,
+-- 	sale_ts TIMESTAMP NOT NULL,
+-- 	payment VARCHAR(200) NOT NULL,
+-- 	cogs NUMERIC (12,2) NOT NULL,
+-- 	gross_income NUMERIC (10,4) NOT NULL,
+-- 	rating NUMERIC (4,1) NOT NULL
+-- );
+
+
+
+--SELECT * FROM raw.sales;
+
+-- DROP TABLE IF EXISTS raw.sales CASCADE;
+-- CREATE TABLE raw.sales(
+-- 	invoice_id TEXT,
+-- 	branch TEXT,
+-- 	city TEXT,
+-- 	customer_type TEXT,
+-- 	gender TEXT,
+-- 	product_line TEXT,
+-- 	unit_price TEXT,
+-- 	quantity TEXT,
+-- 	tax_5pct TEXT,
+-- 	total TEXT,
+-- 	sale_date TEXT,
+-- 	sale_time TEXT,
+-- 	payment TEXT,
+-- 	cogs TEXT,
+-- 	gross_margin_percentage TEXT,
+-- 	gross_income TEXT,
+-- 	rating TEXT
+-- 	);
 
 CREATE SCHEMA IF NOT EXISTS raw;
 CREATE SCHEMA IF NOT EXISTS staging;
@@ -25,4 +48,3 @@ CREATE SCHEMA IF NOT EXISTS dw;
 
 SELECT schema_name
 FROM information_schema.schemata;
-
